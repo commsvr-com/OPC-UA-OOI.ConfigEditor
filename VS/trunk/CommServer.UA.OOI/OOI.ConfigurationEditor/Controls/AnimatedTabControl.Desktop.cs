d27941d17b4cab030741e3f261f75e82d64bd532
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-05-13 13:39:17 +0000

    Architecture improvements - moving UAOOI to separate product. - added keyword to the source files properties. Issue #: 4772

--- VS/trunk/CommServer.UA.OOI/OOI.ConfigurationEditor/Controls/AnimatedTabControl.Desktop.cs
+++ VS/trunk/CommServer.UA.OOI/OOI.ConfigurationEditor/Controls/AnimatedTabControl.Desktop.cs
@@ -1,11 +1,11 @@
 //_______________________________________________________________
 //  Title   : AnimatedTabControl
 //  System  : Microsoft VisualStudio 2015 / C#
-//  $LastChangedDate:  $
-//  $Rev: $
-//  $LastChangedBy: $
-//  $URL: $
-//  $Id:  $
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
 //
 //  Copyright (C) 2016, CAS LODZ POLAND.
 //  TEL: +48 (42) 686 25 47
