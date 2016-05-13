e3c7ba0283fe75b7dff841a12b74301fc1117119
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-05-13 19:03:54 +0000

    Architecture improvements - moving UAOOI to separate product. - added nuget package and version information. Issue #: 4772

--- VS/trunk/CommServer.UA.OOI/OOI.ConfigurationEditor/Properties/AssemblyInfo.cs
+++ VS/trunk/CommServer.UA.OOI/OOI.ConfigurationEditor/Properties/AssemblyInfo.cs
@@ -1,41 +1,30 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: Object Oriented Internet ConfigurationEditor
+//  System  : Microsoft VisualStudio 2013 / C#
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("UAOOI.ConfigurationEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CAS.CommServer.UA.OOI.ConfigurationEditor")]
+[assembly: AssemblyDescription("Object Oriented Internet ConfigurationEditor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UAOOI.ConfigurationEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b8286065-773a-42bc-958b-0dc6366229be")]
+[assembly: AssemblyVersion("1.00.00.*")]
+[assembly: AssemblyFileVersion("1.00.00")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleToAttribute("CAS.CommServerUAOOI.ConfigurationEditor.UnitTest, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3cff1bec656a6" +
-                                                                       "e3784c75cacce8ee3bc695d0ce135d820c86465eb49b2353f0438d28ad9a458e5350c0c141393d" +
-                                                                       "d9ad606bd9f4e0a8e1b0c49fa37835630b16798ece56df81c756f4c6bd37f07d68cc91ebfa7be7" +
-                                                                       "8149d016bbc6a364beda14fc1119698d8c8d9923805b40122bf594f08cc29f41cb0a31e5e141e3" +
-                                                                       "f2421b9d", AllInternalsVisible = true)]
