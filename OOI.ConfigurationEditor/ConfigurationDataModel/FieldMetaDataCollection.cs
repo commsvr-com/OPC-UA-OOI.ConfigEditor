2da297b38dc6ab9e993d3df9a96b5d3be34d7713
Mariusz <mpostol@cas.eu>
2016-05-18 20:57:24 +0000

    CAS.CommServer.UA.OOI.ConfigurationEditor - create and release the package - working on data set model. Issue #: 4830

---
+++ OOI.ConfigurationEditor/ConfigurationDataModel/FieldMetaDataCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UAOOI.Configuration.Networking.Serialization;
+
+namespace CAS.CommServer.UA.OOI.ConfigurationEditor.ConfigurationDataModel
+{
+  public class FieldMetaDataCollection : ObservableCollection<FieldMetaDataWrapper>
+  {
+    private FieldMetaData[] dataSet;
+
+    public FieldMetaDataCollection(FieldMetaData[] dataSet)
+    {
+      this.dataSet = dataSet;
+    }
+  }
+}
---
+++ OOI.ConfigurationEditor/ConfigurationDataModel/FieldMetaDataWrapper.cs
@@ -0,0 +1,40 @@
+﻿//_______________________________________________________________
+//  Title   : Name of Application
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev: $
+//  $LastChangedBy: $
+//  $URL: $
+//  $Id:  $
+//
+//  Copyright (C) 2016, CAS LODZ POLAND.
+//  TEL: +48 (42) 686 25 47
+//  mailto://techsupp@cas.eu
+//  http://www.cas.eu
+//_______________________________________________________________
+
+using UAOOI.Configuration.Networking.Serialization;
+
+namespace CAS.CommServer.UA.OOI.ConfigurationEditor.ConfigurationDataModel
+{
+  public class FieldMetaDataWrapper : Wrapper<FieldMetaData>
+  {
+
+    public FieldMetaDataWrapper(FieldMetaData item) : base(item) { }
+
+    #region Wrapper<FieldMetaData>
+    public override FieldMetaData Item
+    {
+      get
+      {
+        return base.Item;
+      }
+      protected set
+      {
+        base.Item = value;
+      }
+    }
+    #endregion
+
+  }
+}
