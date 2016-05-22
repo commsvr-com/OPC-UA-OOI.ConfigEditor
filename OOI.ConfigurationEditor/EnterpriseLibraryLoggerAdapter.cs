525632077d7d8d42d44354eaa6b9c5dbd7e87a66
Mariusz <mpostol@cas.eu>
2016-05-22 21:00:23 +0000

    CAS.CommServer.UA.OOI.ConfigurationEditor - create and release the package - working on lunching the plug-in - Log initialization must be singleton. The main window must be modal -ShowDialog. The shell is not assigned as the main widow to the application domain. Issue #: 4830

--- OOI.ConfigurationEditor/EnterpriseLibraryLoggerAdapter.cs
+++ OOI.ConfigurationEditor/EnterpriseLibraryLoggerAdapter.cs
@@ -24,26 +24,26 @@ namespace CAS.CommServer.UA.OOI.ConfigurationEditor
 {
   /// <summary>
   /// Class EnterpriseLibraryLoggerAdapter - implements <see cref="ILoggerFacade"/>initializes <see cref="Logger"/> 
-  /// that is the facade for writing a log entry to one or more <see cref="System.Diagnostics.TraceListeners"/>.
+  /// that is the facade for writing a log entry to one or more <see cref="System.Diagnostics.TraceListener"/>.
   /// </summary>
   public class EnterpriseLibraryLoggerAdapter : ILoggerFacade
   {
     /// <summary>
     /// Initializes a new instance of the <see cref="EnterpriseLibraryLoggerAdapter"/> class.
     /// </summary>
-    public EnterpriseLibraryLoggerAdapter()
+    static EnterpriseLibraryLoggerAdapter()
     {
       TextFormatter _extendedFormatter = new TextFormatter("Timestamp: {timestamp}{newline}Message: {message}{newline}Category: {category}{newline}Priority: {priority}{newline}EventId: {eventid}{newline}Severity: {severity}{newline}Title: {title}{newline}Activity ID: {property(ActivityId)}{newline}Machine: {localMachine}{newline}App Domain: {localAppDomain}{newline}ProcessId: {localProcessId}{newline}Process Name: {localProcessName}{newline}Thread Name: {threadName}{newline}Win32 ThreadId:{win32ThreadId}{newline}Extended Properties: {dictionary({key} - {value}{newline})}");
       TextFormatter _briefFormatter = new TextFormatter("{timestamp(local)};{message};{category};{priority};{eventid};{severity};{title}");
 
       // Category Filters
-      ICollection<string> categories = new List<string>();
-      categories.Add("BlockedByFilter");
+      ICollection<string> _categories = new List<string>();
+      _categories.Add("BlockedByFilter");
 
       // Log Filters
       PriorityFilter _priorityFilter = new PriorityFilter("Priority Filter", -1, 99);
       LogEnabledFilter _logEnabledFilter = new LogEnabledFilter("LogEnabled Filter", true);
-      CategoryFilter _categoryFilter = new CategoryFilter("Category Filter", categories, CategoryFilterMode.AllowAllExceptDenied);
+      CategoryFilter _categoryFilter = new CategoryFilter("Category Filter", _categories, CategoryFilterMode.AllowAllExceptDenied);
 
       FlatFileTraceListener _flatFileTraceListener = new FlatFileTraceListener($"{nameof(ConfigurationEditorBase)}.log", string.Empty, string.Empty, _briefFormatter);
       LoggingConfiguration _configuration = new LoggingConfiguration();
