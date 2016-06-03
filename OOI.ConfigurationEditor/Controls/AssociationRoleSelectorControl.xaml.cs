8220875dab88786d5f3305952ca799cf5871f3a3
Mariusz <mpostol@cas.eu>
2016-06-03 09:57:16 +0000

    CAS.CommServer.UA.OOI.ConfigurationEditor - create and release the package - moved role selection to independent user control.
    Issue #: 4830

---
+++ OOI.ConfigurationEditor/Controls/AssociationRoleSelectorControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Controls;
+
+namespace CAS.CommServer.UA.OOI.ConfigurationEditor.Controls
+{
+
+  /// <summary>
+  /// Interaction logic for AssociationRoleSelectorControl.xaml
+  /// </summary>
+  public partial class AssociationRoleSelectorControl : UserControl
+  {
+    public AssociationRoleSelectorControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
--- OOI.ConfigurationEditor/DataSetEditor/DataSetItemConfirmation.cs
+++ OOI.ConfigurationEditor/DataSetEditor/DataSetItemConfirmation.cs
@@ -14,11 +14,11 @@
 //_______________________________________________________________
 
 using CAS.CommServer.UA.OOI.ConfigurationEditor.ConfigurationDataModel;
+using CAS.CommServer.UA.OOI.ConfigurationEditor.Controls;
 using CAS.CommServer.UA.OOI.ConfigurationEditor.ViewModel;
 using CAS.Windows.ViewModel;
 using System;
 using System.Collections.Generic;
-using System.Windows;
 using Serialization = global::UAOOI.Configuration.Networking.Serialization;
 
 namespace CAS.CommServer.UA.OOI.ConfigurationEditor.DataSetEditor.DataSetList
@@ -27,71 +27,17 @@ namespace CAS.CommServer.UA.OOI.ConfigurationEditor.DataSetEditor.DataSetList
   internal class DataSetItemConfirmation : ConfirmationBindable
   {
 
-    internal DataSetItemConfirmation(DataSetConfigurationWrapper wrapper, Func<DataSetConfigurationWrapper, IEnumerable<AssociationCouplerViewModel>> enumerator, bool associationRoleEditable)
+    internal DataSetItemConfirmation
+      (DataSetConfigurationWrapper wrapper, Func<DataSetConfigurationWrapper, IEnumerable<AssociationCouplerViewModel>> enumerator, bool associationRoleEditable)
     {
       DataSetConfigurationWrapper = wrapper;
       m_GetMessageHandlers = enumerator;
       AssociationCouplersEnumerator = m_GetMessageHandlers(DataSetConfigurationWrapper);
-      if (associationRoleEditable)
-        b_AssociationRoleGroupBoxIsEnabled = Visibility.Visible;
-      else
-        b_AssociationRoleGroupBoxIsEnabled = Visibility.Visible;
-      if (AssociationRole == Serialization.AssociationRole.Consumer)
-      {
-        b_ConsumerRoleSelected = true;
-        b_ProducerRoleSelected = false;
-      }
-      else
-      {
-        b_ConsumerRoleSelected = false;
-        b_ProducerRoleSelected = true;
-
-      }
+      AssociationRoleSelectorControlViewModel = new AssociationRoleSelectorControlViewModel(x => AssociationRole = x, AssociationRole, associationRoleEditable);
     }
 
     #region ViewModel API
-
-    private bool? b_ProducerRoleSelected;
-
-    public bool? ProducerRoleSelected
-    {
-      get
-      {
-        return b_ProducerRoleSelected;
-      }
-      set
-      {
-        if (SetProperty<bool?>(ref b_ProducerRoleSelected, value) && value.GetValueOrDefault(false))
-          AssociationRole = Serialization.AssociationRole.Producer;
-      }
-    }
-    private bool? b_ConsumerRoleSelected;
-
-    public bool? ConsumerRoleSelected
-    {
-      get
-      {
-        return b_ConsumerRoleSelected;
-      }
-      set
-      {
-        if (SetProperty<bool?>(ref b_ConsumerRoleSelected, value) && value.GetValueOrDefault(false))
-          AssociationRole = Serialization.AssociationRole.Consumer;
-      }
-    }
-    private Visibility b_AssociationRoleGroupBoxIsEnabled;
-
-    public Visibility AssociationRoleGroupBoxIsEnabled
-    {
-      get
-      {
-        return b_AssociationRoleGroupBoxIsEnabled;
-      }
-      set
-      {
-        SetProperty<Visibility>(ref b_AssociationRoleGroupBoxIsEnabled, value);
-      }
-    }
+    public AssociationRoleSelectorControlViewModel AssociationRoleSelectorControlViewModel { get; }
     public Serialization.AssociationRole AssociationRole
     {
       get
