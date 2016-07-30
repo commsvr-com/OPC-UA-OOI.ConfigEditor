10222ba606b57efdf974ef3a0e107e3f2bc5a9b5
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-07-30 15:14:28 +0000

    Data Domain Discovery - added dialog for resolving URI

---
+++ VS/trunk/CommServer.UA.OOI/OOI.ConfigurationEditor/DomainEditor/DomainModelResolveView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CAS.Windows.ViewModel;
+using System.Windows.Controls;
+
+namespace CAS.CommServer.UA.OOI.ConfigurationEditor.DomainEditor
+{
+  /// <summary>
+  /// Interaction logic for DomainModelResolveView.xaml
+  /// </summary>
+  public partial class DomainModelResolveView : UserControl
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DomainModelResolveView"/> class.
+    /// </summary>
+    public DomainModelResolveView()
+    {
+      InitializeComponent();
+      DataContext = new InteractionRequestAwareBase();
+    }
+  }
+}
